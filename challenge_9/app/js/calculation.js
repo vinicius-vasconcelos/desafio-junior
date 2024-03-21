614c3b56ab5e7c0416bd09093dd45849fd39d309
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-21 11:28:54 -0300

    Melhorando funções de calculo das taxas de juros

--- challenge_9/app/js/calculation.js
+++ challenge_9/app/js/calculation.js
@@ -21,6 +21,26 @@ function removeMaskAndConvertToFloat(value) {
   return floatValue;
 }
 
+function createList(dataObject) {
+  const userInformation = document.getElementById('userInformation');
+  const ul = document.createElement('ul');
+  const messages = {
+    formattedPMT: '- Valor da parcela: ',
+    formattedCET: '- Custo efetivo total: ',
+    formattedIm: '- Taxa efetiva mensal: %',
+    formattedTotal: '- Valor total a ser pago: ',
+  }
+  let li;
+
+  Object.keys(dataObject).forEach((item) => {
+    li = document.createElement('li');
+    li.innerText = `${messages[item]} ${dataObject[item]}`;
+    ul.appendChild(li);
+  });
+
+  userInformation.appendChild(ul);
+}
+
 submit.addEventListener('click', (event) => {
   event.preventDefault();
   const amount = document.getElementById('amount').value;
@@ -37,17 +57,14 @@ submit.addEventListener('click', (event) => {
     const CET = calculateCET(PMT, numberOfInstallmentsParsed, amountWithoutMask);
     const im = calculateEffectiveMonthlyRate(decimalTax);
 
-    const formattedPMT = PMT.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const formattedCET = CET.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const formattedTotal = (CET + amountWithoutMask).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    const language = 'pt-BR';
+    const option = { style: 'currency', currency: 'BRL' }
 
-    document.getElementById('userInformation').innerHTML = `
-    <ul>
-      <li>Valor da parcela: ${formattedPMT}</li>
-      <li>Custo efetivo total: ${formattedCET}</li>
-      <li>Taxa efetiva mensal: ${(im * 100).toFixed(4)} %</li>
-      <li>Valor total a ser pago: ${formattedTotal}</li>
-    </ul>
-    `
+    const formattedPMT = PMT.toLocaleString(language, option);
+    const formattedCET = CET.toLocaleString(language, option);
+    const formattedTotal = (CET + amountWithoutMask).toLocaleString(language, option);
+    const formattedIm = (im * 100).toFixed(2);
+
+    createList({formattedPMT, formattedCET, formattedIm, formattedTotal});
   }
 });

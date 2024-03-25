c1f091723b36c50dbbc2ac6a63b68a2e925e4b66
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-25 09:36:22 -0300

    Documentando melhor o arquivo calculator.js com JSDoc

--- challenge_1/app/calculator.js
+++ challenge_1/app/calculator.js
@@ -1,3 +1,10 @@
+/**
+ * Calcula o resultado de uma operação matemática entre dois números.
+ * @param {string} operator - O operador da operação a ser realizada. Pode ser '1' para adição, '2' para subtração, '3' para multiplicação ou '4' para divisão.
+ * @param {number} number1 - O primeiro número na operação.
+ * @param {number} number2 - O segundo número na operação.
+ * @returns {(number|string)} Retorna o resultado da operação ou uma mensagem de erro, se houver.
+ */
 const calculate = (operator, number1, number2) => {
   if (isNaN(number1) || isNaN(number2)) return 'Erro: Valores de entrada não são numéricos';
   

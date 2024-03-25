2d87fc66dab33eed3c230a535e75fb565268e138
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-25 09:49:08 -0300

    Documentando melhor o arquivo finder.js com JSDoc

--- challenge_2/app/finder.js
+++ challenge_2/app/finder.js
@@ -1,3 +1,8 @@
+/**
+ * Encontra o maior e o menor valor em um array de números.
+ * @param {number[]} arrayOfNumbers - O array de números no qual encontrar o maior e o menor valor. Deve conter exatamente 3 elementos.
+ * @returns {string} Retorna uma mensagem contendo o maior e o menor valor encontrados no array, ou uma mensagem de erro se os valores de entrada não forem numéricos ou se o array não tiver 3 elementos.
+ */
 const findMinMax = (arrayOfNumbers) => {
   const isNumeric = arrayOfNumbers.every((item) => !isNaN(item));
 

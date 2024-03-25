509c2358061cee694b6188f57b4db925aa3e5429
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-25 10:33:05 -0300

    Documentando melhor o arquivo passwordValidator.js com JSDoc

--- challenge_5/app/passwordValidator.js
+++ challenge_5/app/passwordValidator.js
@@ -5,6 +5,11 @@ const hasCapitalLetter = (password) => hasCharacterType(password, /[A-Z]/);
 const hasLowercaseLetter = (password) => hasCharacterType(password, /[a-z]/);
 const hasNumber = (password) => hasCharacterType(password, /\d/);
 
+/**
+ * Verifica se uma senha é válida.
+ * @param {string} password - A senha a ser verificada.
+ * @returns {string} Uma mensagem indicando se a senha é válida ou não.
+ */
 const isValidPassword = (password) => {
   if (!hasValidLength(password)) return 'A senha deve ter no mínimo 8 caracteres';
   if (!hasCapitalLetter(password) || !hasLowercaseLetter(password)) return 'A senha deve conter pelo menos uma letra maiúscula e uma minúscula';

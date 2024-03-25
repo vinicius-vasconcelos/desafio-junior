beeb6fa3f29357e25f2e99a0b885d5dab883005b
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-25 11:21:28 -0300

    Documentando interfaces com JSDoc

--- challenge_7/app/interfaces/IRemoteControl.ts
+++ challenge_7/app/interfaces/IRemoteControl.ts
@@ -1,5 +1,14 @@
 import { Tv } from '../Classes/Tv';
 
+/**
+ * Interface para controle remoto.
+ * Define métodos para controlar uma TV.
+ * @interface
+ * @implements {turnTVOn(tv: Tv): void} - Método de ligar a TV
+ * @implements {turnTVOff(tv: Tv): void} - Método de desligar a TV
+ * @implements {increaseVolume(tv: Tv): void} - Método de aumentar o volume da TV
+ * @implements {decreaseVolume(tv: Tv): void} - Método de diminui o volume da TV
+ */
 interface IRemoteControl {
   turnTVOn(tv: Tv): void;
   turnTVOff(tv: Tv): void;
--- challenge_7/app/interfaces/ITv.ts
+++ challenge_7/app/interfaces/ITv.ts
@@ -1,3 +1,12 @@
+/**
+ * Interface para TV.
+ * Define métodos para controlar uma TV.
+ * @interface
+ * @implements {turnOn(): void} - Método de ligar a TV
+ * @implements {turnOff(): void} - Método de desligar a TV
+ * @implements {increaseVolume(): void} - Método de aumentar o volume da TV
+ * @implements {decreaseVolume(): void} - Método de diminui o volume da TV
+ */
 interface ITv {
   turnOn(): void;
   turnOff(): void;

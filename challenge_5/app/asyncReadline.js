a31dd37bfd61902be2cadef3807e391881353fc4
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-25 10:32:46 -0300

    Documentando melhor o arquivo asyncReadline.js com JSDoc

--- challenge_5/app/asyncReadline.js
+++ challenge_5/app/asyncReadline.js
@@ -5,7 +5,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+/**
+ * Função assíncrona para fazer uma pergunta ao usuário.
+ * @param {string} question - A pergunta a ser exibida ao usuário.
+ * @returns {Promise<string>} Uma promessa que resolve com a entrada do usuário.
+ */
 const asyncQuestion = (question) => new Promise((resolve, _reject) => rl.question(question, (input) => resolve(input)));
+
+/**
+ * Fecha a interface de leitura assíncrona.
+ */
 const closeAsyncQuestion = () => rl.close();
 
 module.exports =  { asyncQuestion, closeAsyncQuestion };

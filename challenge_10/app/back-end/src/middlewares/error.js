f46cba3edcceff447d0fa4638e9aae7eb1a22b7b
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-03-25 14:54:34 -0300

    Documentando funções com JSDoc

--- challenge_10/app/back-end/src/middlewares/error.js
+++ challenge_10/app/back-end/src/middlewares/error.js
@@ -5,6 +5,15 @@ const messageStatusMapping = {
   403: 'Acesso proibido',
 };
 
+/**
+ * Middleware para lidar com erros.
+ * Este middleware é responsável por capturar e lidar com erros durante o processamento da requisição.
+ * @param {Error} error - O erro capturado durante o processamento da requisição.
+ * @param {Object} req - O objeto de requisição express.
+ * @param {Object} res - O objeto de resposta express.
+ * @param {Function} next - A próxima função de middleware na pilha.
+ * @returns {Object} Um objeto de resposta express com um status HTTP e uma mensagem de erro correspondente ao erro capturado.
+ */
 const errorHandler = (error, req, res, next) => {
   if (error.response) {
     const { status } = error.response;
